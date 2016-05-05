ce1f4507076eb55e24684edff2d69c640da33bf5
Keillion <735699921@qq.com>
2016-05-05 10:25:16 +0800

    device test

--- app/src/main/java/github/keillion/planetcloud/DeviceMainFragment.java
+++ app/src/main/java/github/keillion/planetcloud/DeviceMainFragment.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 /**
  * Created by Keillion on 2016/5/2.
@@ -15,7 +16,7 @@ public class DeviceMainFragment extends Fragment implements View.OnClickListener
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
         View view = inflater.inflate(R.layout.fragment_device_main, container, false);
-
+        Toast.makeText(getActivity(),"here is device!",Toast.LENGTH_LONG).show();
         view.findViewById(R.id.navbtn_device_main_favorite).setOnClickListener(this);
         view.findViewById(R.id.navbtn_device_main_frequent).setOnClickListener(this);
         view.findViewById(R.id.navbtn_device_main_room).setOnClickListener(this);
@@ -39,19 +40,19 @@ public class DeviceMainFragment extends Fragment implements View.OnClickListener
         FragmentTransaction ft=fm.beginTransaction();
         switch (v.getId()){
             case R.id.navbtn_device_main_favorite:
-                ft.replace(R.id.content_main,new SceneMainFragment());
+                ft.replace(R.id.content_main,null);
                 break;
             case R.id.navbtn_device_main_frequent:
-                ft.replace(R.id.content_main,new SceneMainFragment());
+                ft.replace(R.id.content_main,null);
                 break;
             case R.id.navbtn_device_main_room:
-                ft.replace(R.id.content_main,new SceneMainFragment());
+                ft.replace(R.id.content_main,null);
                 break;
             case R.id.navbtn_device_main_class:
-                ft.replace(R.id.content_main,new SceneMainFragment());
+                ft.replace(R.id.content_main,null);
                 break;
             case R.id.navbtn_device_main_add:
-                ft.replace(R.id.content_main,new SceneMainFragment());
+                ft.replace(R.id.content_main,null);
                 break;
         }
 
--- app/src/main/java/github/keillion/planetcloud/MainActivity.java
+++ app/src/main/java/github/keillion/planetcloud/MainActivity.java
@@ -5,6 +5,7 @@ import android.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -14,6 +15,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(getApplicationContext(),"It is loaded!",Toast.LENGTH_LONG).show();
         findViewById(R.id.navbtn_main_scene).setOnClickListener(this);
         findViewById(R.id.navbtn_main_device).setOnClickListener(this);
         findViewById(R.id.navbtn_main_plan).setOnClickListener(this);
@@ -35,16 +37,19 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         FragmentTransaction ft=fm.beginTransaction();
         switch (v.getId()){
             case R.id.navbtn_main_scene:
+                Toast.makeText(getApplicationContext(),"scene is clicked!",Toast.LENGTH_LONG).show();
                 ft.replace(R.id.content_main,new SceneMainFragment());
                 break;
             case R.id.navbtn_main_device:
+                Toast.makeText(getApplicationContext(),"device is clicked!",Toast.LENGTH_LONG).show();
                 ft.replace(R.id.content_main,new DeviceMainFragment());
                 break;
             case R.id.navbtn_main_plan:
-                ft.replace(R.id.content_main,new SceneMainFragment());
+                Toast.makeText(getApplicationContext(),"plan is clicked!",Toast.LENGTH_LONG).show();
+                ft.replace(R.id.content_main,null);
                 break;
             case R.id.navbtn_main_setting:
-                ft.replace(R.id.content_main,new SceneMainFragment());
+                ft.replace(R.id.content_main,null);
                 break;
         }
     }
